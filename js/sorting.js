e11173bb1934f7f302972e63cbfe5eacf93d11a1
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-09-20 22:22:01 +0200

    refactoring sorting js

--- js/sorting.js
+++ js/sorting.js
@@ -12,50 +12,26 @@
     marketingItems: document.querySelectorAll('[marketing]'),
   };
 
-  refs.allBtn.addEventListener('click', showAll);
-  refs.websiteBtn.addEventListener('click', showWebsite);
-  refs.appBtn.addEventListener('click', showApp);
-  refs.designBtn.addEventListener('click', showDesign);
-  refs.marketingBtn.addEventListener('click', showMarketing);
+  refs.allBtn.addEventListener('click', () => refs.items.forEach(e=>e.classList.remove('is-hidden')));
+  
+  refs.websiteBtn.addEventListener('click', () => {
+    refs.items.forEach(e => e.classList.add('is-hidden'));
+    refs.websiteItems.forEach(e => e.classList.remove('is-hidden'))
+  });
 
-  function showAll() {
-    for (i = 0; i < refs.items.length; i++){
-       refs.items[i].classList.remove('is-hidden');
-    }
-  }
+  refs.appBtn.addEventListener('click', () => {
+    refs.items.forEach(e => e.classList.add('is-hidden'));
+    refs.appItems.forEach(e => e.classList.remove('is-hidden'))
+  });
+  
+  refs.designBtn.addEventListener('click', () => {
+    refs.items.forEach(e => e.classList.add('is-hidden'));
+    refs.designItems.forEach(e => e.classList.remove('is-hidden'))
+  });
 
-  function hideAll() {
-    for (i = 0; i < refs.items.length; i++){
-       refs.items[i].classList.add('is-hidden');
-    }
-  }
-
-  function showWebsite(){
-    hideAll();
-    for (i = 0; i < refs.websiteItems.length; i++){
-       refs.websiteItems[i].classList.remove('is-hidden');
-    }
-  }
-
-  function showApp(){
-    hideAll();
-    for (i = 0; i < refs.appItems.length; i++){
-       refs.appItems[i].classList.remove('is-hidden');
-    }
-  }
-
-  function showDesign(){
-    hideAll();
-    for (i = 0; i < refs.designItems.length; i++){
-       refs.designItems[i].classList.remove('is-hidden');
-    }
-  }
-
-  function showMarketing(){
-    hideAll();
-    for (i = 0; i < refs.marketingItems.length; i++){
-       refs.marketingItems[i].classList.remove('is-hidden');
-    }
-  }
+  refs.marketingBtn.addEventListener('click', () => {
+    refs.items.forEach(e => e.classList.add('is-hidden'));
+    refs.marketingItems.forEach(e => e.classList.remove('is-hidden'))
+  });  
 
 })();
